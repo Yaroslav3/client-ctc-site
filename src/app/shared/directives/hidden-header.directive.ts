e15e41ff58d595ed069ab98e48abf88554e8cf62
Vetal <vitaliy_klimashe@ukr.net>
2020-01-20 17:53:41 +0200

    refactor trainings page update 2

---
+++ src/app/shared/directives/hidden-header.directive.ts
@@ -0,0 +1,11 @@
+import {Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appHiddenHeader]'
+})
+export class HiddenHeaderDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'display', 'none');
+  }
+
+}
